cd72c9b769d055563b766275703f742548806e6a
Manuraj <manuraj.m@tryzens.com>
2023-11-28 09:32:06 +0530

    Removed empty try catch and update typescript version

--- web/src/lib/createPayment.ts
+++ web/src/lib/createPayment.ts
@@ -2,37 +2,33 @@ import { createPayment } from '@worldline/app-integration';
 import { Request } from './types';
 
 export async function createPaymentRequest(request: Request) {
-  try {
-    const { storeId, hostedTokenizationId, returnUrl } = request.body;
+  const { storeId, hostedTokenizationId, returnUrl } = request.body;
 
-    if (!storeId || !hostedTokenizationId || !returnUrl) {
-      throw {
-        message: 'Required parameters are missing or empty',
-        statusCode: 400,
-      };
-    }
-
-    const { authorization: authToken } = request.headers;
-
-    if (!authToken) {
-      throw {
-        message: 'Authentication parameters are missing or empty',
-        statusCode: 403,
-      };
-    }
-
-    // Perform create payment request to app
-    const options = {
-      authToken,
-      storeId,
-      hostedTokenizationId,
-      returnUrl,
+  if (!storeId || !hostedTokenizationId || !returnUrl) {
+    throw {
+      message: 'Required parameters are missing or empty',
+      statusCode: 400,
     };
-
-    const result = await createPayment(options);
-
-    return result;
-  } catch (error) {
-    throw error;
   }
+
+  const { authorization: authToken } = request.headers;
+
+  if (!authToken) {
+    throw {
+      message: 'Authentication parameters are missing or empty',
+      statusCode: 403,
+    };
+  }
+
+  // Perform create payment request to app
+  const options = {
+    authToken,
+    storeId,
+    hostedTokenizationId,
+    returnUrl,
+  };
+
+  const result = await createPayment(options);
+
+  return result;
 }
