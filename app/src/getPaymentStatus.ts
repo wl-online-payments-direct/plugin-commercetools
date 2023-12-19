3db38c27ee8d4570a88cc71db48207b39995db2b
Manuraj <manuraj.m@tryzens.com>
2023-12-19 15:15:34 +0530

    Update payment status api with id

--- app/src/getPaymentStatus.ts
+++ app/src/getPaymentStatus.ts
@@ -1,14 +1,24 @@
 import { getCustomObjects } from '@worldline/ctintegration-ct';
 import { getPaymentStatusService } from '@worldline/ctintegration-psp';
+import { getPayment } from '@worldline/ctintegration-db';
 import { GetPaymentStatusPayload } from './types';
-import { getConnectionServiceProps } from './mappers';
-import { getPaymentStatusPayload } from './mappers/getPaymentStatus';
+import {
+  getConnectionServiceProps,
+  getPaymentStatusDBPayload,
+} from './mappers';
 
 export async function getPaymentStatus(payload: GetPaymentStatusPayload) {
+  const payment = await getPayment(getPaymentStatusDBPayload(payload));
+  if (!payment) {
+    throw {
+      message: `Failed to fetch the payment for id : '${payload.id}'`,
+      statusCode: 500,
+    };
+  }
   // Prepare service payload for get payment status
   const paymentServiceResponse = await getPaymentStatusService(
     getConnectionServiceProps(await getCustomObjects(payload.storeId)),
-    getPaymentStatusPayload(payload),
+    payment.worldlineId,
   );
   // Response only have status
   return paymentServiceResponse;
--- web/src/lib/mapper/getPaymentStatus.ts
+++ web/src/lib/mapper/getPaymentStatus.ts
@@ -3,19 +3,20 @@ import { QueryParams, Request } from '../types';
 import { pick } from './common';
 
 export function getPaymentStatusRequiredProps(queryString: QueryParams) {
-  return pick(queryString, ['paymentId', 'storeId']);
+  return pick(queryString, ['id', 'storeId']);
 }
 
 export function getPaymentStatusAppPayload(
   request: Request,
   queryString: QueryParams,
 ) {
-  const { authorization: authToken = '' } = request.headers;
+  const id = queryString.id?.toString();
   const storeId = queryString.storeId?.toString();
-  const paymentId = queryString.paymentId?.toString();
+  const { authorization: authToken = '' } = request.headers;
+
   return {
-    authToken,
+    id,
     storeId,
-    paymentId,
+    authToken,
   };
 }
