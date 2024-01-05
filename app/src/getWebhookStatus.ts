bf9ce373ba1a82ed550c1880f160b75edfe7c3ec
Manuraj <manuraj.m@tryzens.com>
2024-01-05 12:56:42 +0530

    Added cart invoke for authentication

--- app/src/getWebhookStatus.ts
+++ app/src/getWebhookStatus.ts
@@ -1,4 +1,5 @@
 import { getPayment } from '@worldline/ctintegration-db';
+import { getMyCart } from '@worldline/ctintegration-ct';
 import { GetWebhookStatusPayload } from './types';
 import {
   getWebhookStatusDBQuery,
@@ -6,6 +7,14 @@ import {
 } from './mappers';
 
 export async function getWebhookStatus(payload: GetWebhookStatusPayload) {
+  // Fetch cart from Commercetools to authenticate
+  const { cart } = await getMyCart(payload.authToken);
+  if (!cart) {
+    throw {
+      message: 'Failed to fetch the cart of cart is missing',
+      statusCode: 500,
+    };
+  }
   // Invoke db to get payment status
   const payment = await getPayment(getWebhookStatusDBQuery(payload));
 
