951c5f6acadfbca0f045c4e24f627258b9bb1c9d
Manuraj <manuraj.m@tryzens.com>
2024-01-08 14:35:10 +0530

    Removed pick function

--- app/src/mappers/loadPaymentMethods.ts
+++ app/src/mappers/loadPaymentMethods.ts
@@ -1,18 +1,5 @@
 import { CustomObjects, CustomerPaymentToken } from '../types';
 
-export function pick<Data extends object, Keys extends keyof Data>(
-  data: Data,
-  keys: Keys[],
-): Pick<Data, Keys> {
-  const result = {} as Pick<Data, Keys>;
-
-  keys.forEach((key) => {
-    result[key] = data[key];
-  });
-
-  return result;
-}
-
 export function loadPaymentMethodsMappedResponse(
   customConfig: CustomObjects,
   customerPaymentTokens: CustomerPaymentToken[] | null,
