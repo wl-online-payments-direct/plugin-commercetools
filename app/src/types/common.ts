e2042e9d545cbcb127abf266c21645b15487404b
Manuraj <manuraj.m@tryzens.com>
2024-02-02 14:07:42 +0530

    Modified redirect mode A format

--- app/src/types/common.ts
+++ app/src/types/common.ts
@@ -1,9 +1,31 @@
-export interface CheckoutProps {
-  label: string;
-  logo: string;
+export interface RedirectModeAPaymentOptions {
   enabled: boolean;
-  payButtonTitle?: string;
+  merchantReferenceID: string;
+  paymentOptions: {
+    displayOrder: number;
+    label: string;
+    enabled: boolean;
+    logo: string;
+  }[];
 }
+
+export interface RedirectModeBPaymentOptions {
+  enabled: boolean;
+  logo: string;
+  payButtonTitle: string;
+  merchantReferenceID: string;
+  templateFileName: string;
+}
+
+export interface OnSiteModePaymentOptions {
+  enabled: boolean;
+  logo: string;
+  payButtonTitle: string;
+  payButtonLanguage: string;
+  merchantReferenceID: string;
+  templateFileName: string;
+}
+
 export interface CustomObjects {
   mode: string;
   authorizationMode: string;
@@ -19,16 +41,15 @@ export interface CustomObjects {
   webhookSecret: string;
   webhookUrl: string;
   redirectUrl: string;
-  redirectModeA_payOptionUpdate: {
-    [key: string]: CheckoutProps;
-  };
-  redirectModeB: CheckoutProps;
-  onSiteMode: CheckoutProps;
+  redirectModeA: RedirectModeAPaymentOptions;
+  redirectModeB: RedirectModeBPaymentOptions;
+  onSiteMode: OnSiteModePaymentOptions;
 }
 
 export interface PaymentMethod {
   name: string;
   type: string;
+  displayOrder?: number;
   image: { src: string };
   enabled: boolean;
   paymentMethod: string;
